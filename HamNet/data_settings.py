894dae9ba843774013e7dc0522630b14db64a9b0
fjssharpsword <sharpsword@163.com>
2022-07-07 14:48:35 +0800

    0707

---
+++ HamNet/data_settings.py
@@ -0,0 +1,6 @@
+# Data dir & Model dir
+DATA_ROOT = '/data/fjsdata/VOC2012/VOCdevkit/VOC2012'
+TEST_DATA_ROOT = 'PATH_TO_YOUR_DATA/VOC/TestSet'
+
+MODEL_DIR = '/data/tmpexec/pretrained_model/'
+TEST_SAVE_DIR = '/data/pycode/SFSAttention/HamNet/test_results'
---
+++ HamNet/hamburger/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Hamburger for Pytorch
+
+@author: Gsunshine
+"""
+from .bread import ConvBNReLU
+from .burger import get_hamburger
